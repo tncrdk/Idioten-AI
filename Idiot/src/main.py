0998434ac938ebfdd318b5393e4e36bf946eb959
tncrdk <85764203+tncrdk@users.noreply.github.com>
2021-10-30 15:38:53 +0200

    legger til flere paths til packages

--- Idiot/src/main.py
+++ Idiot/src/main.py
@@ -1,8 +1,11 @@
 import sys
 
-sys.path.append(
-    r"C:\Users\thorb\Documents\Python\TekForsk\Idioten Prosjekt\Idiot\GameEngine"
-)
+paths_to_include = [
+    r"C:\Users\thorb\Documents\Python\TekForsk\Idioten Prosjekt\Idiot\GameEngine",
+    r"C:\Users\thorb\Documents\Python\TekForsk\Idioten Prosjekt\Idiot\Agents\AI_agents",
+    r"C:\Users\thorb\Documents\Python\TekForsk\Idioten Prosjekt\Idiot\Agents\Static_agents",
+]
+sys.path += paths_to_include
 
 import game_engine as ge
 
