49c38f2326cf0af6e797d63f25faacc2776fad7f
tncrdk <85764203+tncrdk@users.noreply.github.com>
2022-01-17 19:00:51 +0100

    endring av stdout

--- Idiot/src/main.py
+++ Idiot/src/main.py
@@ -44,11 +44,13 @@ neat_agent = create_NEAT_agent(config_path, genome_path)
 
 
 agents1 = [sa.PlayLowAgent1(), sa.PlayLowAgent1()]
-agents2 = [sa.PlayLowSaveAgent1("b"), neat_agent]
+agents2 = [neat_agent, sa.PlayLowSaveAgent1("b")]
 
 results = {agents2[0].name: 0, agents2[1].name: 0}
 games = 10000
 tot_turns = 0
+
+start_time = time.time()
 
 for i in range(games):
     main_game = ge.AgentGame(agents=agents2, run_game=False)
@@ -57,7 +59,9 @@ for i in range(games):
         results[standings.name] += 1
     tot_turns += turns
 
-# main_game = ge.AgentGame(agents=agents2)
+tot_time = time.time() - start_time
+
 print(results[agents2[0].name] / games)
 print(results[agents2[1].name] / games)
 print(tot_turns / games)
+print(f"Total tid: {tot_time}")
