76949c9a739f53662165ea4df8c23489474ea885
tncrdk <85764203+tncrdk@users.noreply.github.com>
2022-05-12 17:51:57 +0200

    installerte pandas

--- Idiot/src/test.py
+++ Idiot/src/test.py
@@ -3,21 +3,19 @@ import game_engine as ge
 import card_switch as cs
 import deck
 import static_agents as sa
-import math
-import os
+import json
+import numpy as np
+import pandas as pd
+
+l = [[[1, 2, 3], [4, 5, 6]], [[1, 2, 34], [4564, 456]]]
+
+a = np.array(l)
+with open("test.json", "w") as f:
+    json.dump(l, f)
+    json.dump(l, f)
+    print(l)
 
 
-def choose_card(output_data, playable_cards):
-    output_data = list(enumerate(output_data))
-    output_data.sort(key=lambda x: x[1], reverse=True)
-
-    for index, _ in output_data:
-        for i, card in playable_cards:
-            if index + 2 == card:
-                return card, i
-
-
-playable_cards = [(0, 3), (1, 6), (2, 8), (3, 9), (4, 9), (5, 9)]
-output_data = [1, 2, 3, 4, 4]
-
-print(choose_card(output_data, playable_cards))
+# with open("test.json", "r") as f:
+#     data = json.load(f)
+#     print(data)
