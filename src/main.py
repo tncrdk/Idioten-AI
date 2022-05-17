60bae277889c2017e50d8f1b52d003458fd5f482
tncrdk <85764203+tncrdk@users.noreply.github.com>
2022-05-17 14:38:41 +0200

    main.py lagt til for sentralisering

---
+++ src/main.py
@@ -0,0 +1,26 @@
+import turn_analysis as ta
+import winrate_analysis as wa
+
+
+def win_analysis():
+    LOG_WINS_PATH = r".\Log\log_neat_first_results.txt"
+    MODE = "binomial"
+
+    analyzer = wa.WinRateAnalysis()
+    analyzer.analyze(LOG_WINS_PATH, MODE)
+
+
+def turn_analysis():
+    LOG_TURNS_PATH = r".\Log\log_turns.txt"
+    MODE = "binomial"
+
+    analyzer = ta.TurnAnalysis(LOG_TURNS_PATH, 1000, 100)
+    analyzer.analyze(MODE)
+
+
+def main():
+    win_analysis()
+
+
+if __name__ == "__main__":
+    main()
